cf85a76854e620222e2e8ec255921f5e11c04d80
Rayuchida1999 <rayuchida1999@gmail.com>
2025-06-18 21:05:36 +0900

    Implement basic QA bot functionality with Flask, including question handling and response rendering

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template, jsonify
+import os
+from quetion import dict_bot
+
+app = Flask(
+    __name__,
+    static_folder='static',
+    template_folder='templates'
+)
+
+@app.route('/')
+def home():
+    return render_template('Main.html')
+
+@app.route('/ask', methods=['POST'])
+def ask():
+    data = request.get_json()
+    question = data.get('question', '')
+    answer = dict_bot(question)
+    return jsonify({'answer': answer})
